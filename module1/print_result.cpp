0b18da5939a70a0e42f576b7c8b9dd92f5033e3e
jtG <jude.gyimah87@gmail.com>
2021-01-06 06:00:57 +0100

    module 1 to 9

---
+++ module1/print_result.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print_results(std::string result_type, float result_value){
+    std::cout << result_type << "result:\t" << result_value << "\n";
+}
---
+++ module3/build/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jtg/Desktop/cmake_tutorial/module3/main.cpp" "/home/jtg/Desktop/cmake_tutorial/module3/build/CMakeFiles/calculator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../my_math_dir/include"
+  "../my_print_dir/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jtg/Desktop/cmake_tutorial/module3/build/my_math_dir/CMakeFiles/my_math.dir/DependInfo.cmake"
+  "/home/jtg/Desktop/cmake_tutorial/module3/build/my_print_dir/CMakeFiles/my_print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
